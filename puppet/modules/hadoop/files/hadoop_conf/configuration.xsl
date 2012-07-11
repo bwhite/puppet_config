6bff9d51216fb1690d788e00c44a52be81e5162d
Brandyn A. White <bwhite@dappervision.com>
2012-07-11 14:33:29 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- puppet/modules/hadoop/files/hadoop_conf/configuration.xsl
+++ puppet/modules/hadoop/files/hadoop_conf/configuration.xsl
@@ -1,29 +1,24 @@
-from __future__ import absolute_import
-
-from celery.app.annotations import MapAnnotation, prepare
-from celery.task import task
-from celery.utils.imports import qualname
-
-from celery.tests.utils import Case
-
-
-@task()
-def add(x, y):
-    return x + y
-
-
-@task()
-def mul(x, y):
-    return x * y
-
-
-class MyAnnotation(object):
-    foo = 65
-
-
-class test_MapAnnotation(Case):
-
-    def test_annotate(self):
-        x = MapAnnotation({add.name: {'foo': 1}})
-        self.assertDictEqual(x.annotate(add), {'foo': 1})
-        self.assertIsNone(x.an
+<?xml version="1.0"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+<xsl:output method="html"/>
+<xsl:template match="configuration">
+<html>
+<body>
+<table border="1">
+<tr>
+ <td>name</td>
+ <td>value</td>
+ <td>description</td>
+</tr>
+<xsl:for-each select="property">
+<tr>
+  <td><a name="{name}"><xsl:value-of select="name"/></a></td>
+  <td><xsl:value-of select="value"/></td>
+  <td><xsl:value-of select="description"/></td>
+</tr>
+</xsl:for-each>
+</table>
+</body>
+</html>
+</xsl:template>
+</xsl:stylesheet>
--- puppet/modules/picarus/manifests/init.pp
+++ puppet/modules/picarus/manifests/init.pp
@@ -1,112 +1,102 @@
-# -*- coding: utf-8 -*-
-"""
-    celery.task.http
-    ~~~~~~~~~~~~~~~~
-
-    Webhook task implementation.
-
-"""
-from __future__ import absolute_import
-
-import anyjson
-import sys
-import urllib2
-
-from urllib import urlencode
-from urlparse import urlparse
-try:
-    from urlparse import parse_qsl
-except ImportError:  # pragma: no cover
-    from cgi import parse_qsl  # noqa
-
-from celery import __version__ as celery_version
-from celery.utils.log import get_task_logger
-from .base import Task as BaseTask
-
-GET_METHODS = frozenset(['GET', 'HEAD'])
-logger = get_task_logger(__name__)
-
-
-class InvalidResponseError(Exception):
-    """The remote server gave an invalid response."""
-
-
-class RemoteExecuteError(Exception):
-    """The remote task gave a custom error."""
+class picarus_pre {
+  exec {'pip install -q argparse':
+      path => ['/usr/local/bin', '/opt/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+      unless => 'python -c "import argparse"',
+  }
+  package {
+      "unittest2":
+          ensure => latest,     
+          provider => pip;
+      "pil":
+          ensure => latest,     
+          provider => pip;
+      "thrift":  # TODO(brandyn): This belongs in the hadoop module
+          ensure => latest,     
+          provider => pip;
+      "cython":
+          ensure => "0.16",
+          provider => pip;
+      "numpy":
+          ensure => "1.6.2",
+          provider => pip;
+      "scipy":
+          ensure => "0.10.1",
+          provider => pip,
+          require => Package["numpy"];
+      "scikit-learn":
+          ensure => "0.11",
+          provider => pip,
+          require => Package["numpy", "scipy"];
+  }
+}
 
 
-class UnknownStatusError(InvalidResponseError):
-    """The remote server gave an unknown status."""
+class picarus {
+  $b = "git+https://github.com/bwhite/"
 
+  exec {'wget http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.2/OpenCV-2.4.2.tar.bz2 && tar -xjf OpenCV-2.4.2.tar.bz2 && mkdir OpenCV-2.4.2/build && cd OpenCV-2.4.2/build && cmake -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF .. && make -j4 && make install':
+      path => ['/usr/local/bin', '/opt/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+      unless => 'python -c "import cv2"',
+      logoutput => true,
+      timeout => 1800
+  }
 
-def maybe_utf8(value):
-    """Encode to utf-8, only if the value is Unicode."""
-    if isinstance(value, unicode):
-        return value.encode('utf-8')
-    return value
-
-
-if sys.version_info[0] == 3:  # pragma: no cover
-
-    def utf8dict(tup):
-        if not isinstance(tup, dict):
-            return dict(tup)
-        return tup
-else:
-
-    def utf8dict(tup):  # noqa
-        """With a dict's items() tuple return a new dict with any utf-8
-        keys/values encoded."""
-        return dict((key.encode('utf-8'), maybe_utf8(value))
-                        for key, value in tup)
-
-
-def extract_response(raw_response, loads=anyjson.loads):
-    """Extract the response text from a raw JSON response."""
-    if not raw_response:
-        raise InvalidResponseError('Empty response')
-    try:
-        payload = loads(raw_response)
-    except ValueError, exc:
-        raise InvalidResponseError, InvalidResponseError(
-                str(exc)), sys.exc_info()[2]
-
-    status = payload['status']
-    if status == 'success':
-        return payload['retval']
-    elif status == 'failure':
-        raise RemoteExecuteError(payload.get('reason'))
-    else:
-        raise UnknownStatusError(str(status))
-
-
-class MutableURL(object):
-    """Object wrapping a Uniform Resource Locator.
-
-    Supports editing the query parameter list.
-    You can convert the object back to a string, the query will be
-    properly urlencoded.
-
-    Examples
-
-        >>> url = URL('http://www.google.com:6580/foo/bar?x=3&y=4#foo')
-        >>> url.query
-        {'x': '3', 'y': '4'}
-        >>> str(url)
-        'http://www.google.com:6580/foo/bar?y=4&x=3#foo'
-        >>> url.query['x'] = 10
-        >>> url.query.update({'George': 'Costanza'})
-        >>> str(url)
-        'http://www.google.com:6580/foo/bar?y=4&x=10&George=Costanza#foo'
-
-    """
-    def __init__(self, url):
-        self.parts = urlparse(url)
-        self.query = dict(parse_qsl(self.parts[4]))
-
-    def __str__(self):
-        scheme, netloc, path, params, query, fragment = self.parts
-        query = urlencode(utf8dict(self.query.items()))
-        components = [scheme + '://', netloc, path or '/',
-                      ';%s' % params   if params   else '',
-                      '?%s' % query    
+  package {
+      "image_server":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}image_server.git";
+      "static_server":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}static_server.git";
+      "imfeat":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}imfeat.git";
+      "impoint":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}impoint.git";
+      "distpy":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}distpy.git";
+      "kernels":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}kernels.git";
+      "keyframe":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}keyframe.git";
+      "viderator":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}viderator.git";
+      "vidfeat":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}vidfeat.git";
+      "hadoopy":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}hadoopy.git";
+      "hadoopy_hbase":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}hadoopy_hbase.git";
+      "hadoopy_flow":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}hadoopy_flow.git";
+      "picarus":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}picarus.git";
+      "vision_data":
+          ensure => latest,     
+          provider => pip,
+          source => "${b}vision_data.git";
+  }
+}
