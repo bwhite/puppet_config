28325886706ea82c8a267cce98f88f932a754e81
Brandyn A. White <bwhite@dappervision.com>
2012-07-24 17:31:51 -0400

    1. Added pmaster option to genconfig 2. Master/slave setup scripts now uncomment commented repos in sources.list Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- genconfig.py
+++ genconfig.py
@@ -4,7 +4,9 @@ import time
 import glob
 
 
-def run(master, slaves):
+def run(master, slaves, pmaster):
+    if pmaster is None:
+        pmaster = master
     root = 'puppet'
     shutil.rmtree(root, ignore_errors=True)
     shutil.copytree('puppet_src', root)
@@ -27,23 +29,23 @@ def run(master, slaves):
     fn = '%s/puppet_client_setup.sh' % root
     with open(fn) as fp:
         data = fp.read()
-        data = data.replace('{{master}}', "%s" % master)
+        data = data.replace('{{master}}', "%s" % master).replace('{{pmaster}}', "%s" % pmaster)
         print('Writing[%s]' % fn)
         with open(fn, 'w') as fp:
             fp.write(data)
     fn = '%s/puppet.conf' % root
     with open(fn) as fp:
         data = fp.read()
-        data = data.replace('{{master}}', "%s" % master)
+        data = data.replace('{{master}}', "%s" % master).replace('{{pmaster}}', "%s" % pmaster)
         print('Writing[%s]' % fn)
         with open(fn, 'w') as fp:
             fp.write(data)
 
-    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Setup puppet dir")
     # Server port
-    parser.add_argument('master')
-    parser.add_argument('slaves', nargs='+')
+    parser.add_argument('master', help="Hadoop master (JobTracker, NameNode, TaskTracker, DataNode)")
+    parser.add_argument('slaves', nargs='+', help="Hadoop slaves (TaskTracker, DataNode)")
+    parser.add_argument('--pmaster', help="Puppet master server (if unspecified, master is used)")
     run(**vars(parser.parse_args()))
--- puppet_src/puppet_client_setup.sh
+++ puppet_src/puppet_client_setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # install necessary packages
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cat /etc/apt/sources.list.bak | sed 's/#deb/deb/g' > /etc/apt/sources.list
 apt-get update
 apt-get install -y libopenssl-ruby rdoc libopenssl-ruby1.8 libreadline-ruby1.8 libruby1.8 rdoc1.8 ruby1.8
 # Install facter (used by puppet to determine system config)
-wget http://downloads.puppetlabs.com/facter/facter-1.6.9.tar.gz
+wget -N http://downloads.puppetlabs.com/facter/facter-1.6.9.tar.gz
 tar -xzf facter-1.6.9.tar.gz
 cd facter-1.6.9
 ruby1.8 install.rb
 cd ..
 # Install puppet
-wget http://downloads.puppetlabs.com/puppet/puppet-2.7.14.tar.gz
+wget -N http://downloads.puppetlabs.com/puppet/puppet-2.7.14.tar.gz
 tar -xzf puppet-2.7.14.tar.gz
 cd puppet-2.7.14
 ruby1.8 install.rb
@@ -18,5 +20,5 @@ ruby1.8 install.rb
 mkdir -p /etc/puppet
 echo "[main]" > /etc/puppet/puppet.conf
 echo "certname = " `ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' | head -n 1` >> /etc/puppet/puppet.conf
-echo "server = {{master}}" >> /etc/puppet/puppet.conf
+echo "server = {{pmaster}}" >> /etc/puppet/puppet.conf
 puppet agent -t --debug
