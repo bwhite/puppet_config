a543bd5e5d36113ddddfa92020d332f83fdc6543
Brandyn A. White <bwhite@dappervision.com>
2012-07-25 10:00:50 -0400

    Added fab file
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

---
+++ fabfile.py
@@ -0,0 +1,31 @@
+from fabric.api import run, sudo, put, get, env
+from fabric.context_managers import cd, settings
+import time
+import os
+import contextlib
+
+
+@contextlib.contextmanager
+def mkdir(prefix):
+    work_dir = '%s-%f' % (prefix, time.time())
+    run('mkdir %s' % work_dir)
+    try:
+        with cd(work_dir):
+            yield
+    finally:
+        sudo('rm -r %s' % work_dir)
+
+
+def setup():
+    put('puppet/puppet_client_setup.sh', '.')
+    sudo('bash puppet_client_setup.sh')
+
+
+def agent():
+    sudo('puppet agent --onetime --verbose --ignorecache --no-daemonize --no-usecacheonfailure --no-splay')
+
+
+def rmcerts():
+    for host in env['hosts']:
+        print('You should execute this on the puppet master "sudo puppet cert clean %s"' % host)
+    sudo('rm -f /etc/puppet/ssl/certs/*.pem')
